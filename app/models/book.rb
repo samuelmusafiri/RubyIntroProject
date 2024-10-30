35fb81049b00b9f6dacc3171548fa3071c6aa5e5
Samuel Musafiri <smusafiri@academic.rrc.ca>
2024-10-30 14:12:24 -0500

    2nd Validation for the book model (valid interger validation)

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,6 +3,7 @@ class Book < ApplicationRecord
   validates :title, :published_date, uniqueness: true, presence: true 
   #Presence ensures the title is not empty
   #Uniqueness prevent duplication
-  validates :published_year, numericality: { only_integer: true, less_than_or_equal_to: Time.now.year } #Ensures the publication date is a valid intergers
+  validates :published_year, numericality: { only_integer: true, less_than_or_equal_to: Time.now.year } 
+  #Ensures the publication date is a valid interger
 end
 
