7704b9ec8aa9475a36e3b08ca361d5f2e82b329e
Samuel Musafiri <smusafiri@academic.rrc.ca>
2024-10-30 14:15:16 -0500

    2nd validation for author model (Character length validation)

--- app/models/author.rb
+++ app/models/author.rb
@@ -2,6 +2,7 @@ class Author < ApplicationRecord
     has_many :books, dependent: :destroy
     validates :name, presence: true 
     #Presence ensures that authors name is present 
-    validates :biography, length: { maximum: 500 } #Limitation on characters to avoid long text
+    validates :biography, length: { maximum: 500 } 
+    #Limitation on characters to avoid long text
   end
   
