6f4a0b4a43e5512b6b0c6a79de10c8330b454262
Samuel Musafiri <smusafiri@academic.rrc.ca>
2024-10-30 14:10:14 -0500

    First validation for the book model (title validation)

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,8 @@
 class Book < ApplicationRecord
   belongs_to :author
-  validates :title, :published_date, uniqueness: true, presence: true #Presence Ensures the title is not empty 
-  #uniqueness prevents duplication
-  validates :published_year, numericality: { only_integer: true, less_than_or_equal_to: Time.now.year } #Ensures the publication date is a valid interger
+  validates :title, :published_date, uniqueness: true, presence: true 
+  #Presence ensures the title is not empty
+  #Uniqueness prevent duplication
+  validates :published_year, numericality: { only_integer: true, less_than_or_equal_to: Time.now.year } #Ensures the publication date is a valid intergers
 end
 
