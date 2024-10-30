e446b4eaad7da52bb6f5f4ab6c25673ed5dd86e6
Samuel Musafiri <smusafiri@academic.rrc.ca>
2024-10-30 14:13:55 -0500

    1st validation for author model (name presence validation)

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,6 +1,7 @@
 class Author < ApplicationRecord
     has_many :books, dependent: :destroy
-    validates :name, presence: true #Presence ensures that authors name is present
+    validates :name, presence: true 
+    #Presence ensures that authors name is present 
     validates :biography, length: { maximum: 500 } #Limitation on characters to avoid long text
   end
   
